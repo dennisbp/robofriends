56673a9b0380c34852d6ceb72420636e6ffb6e54
Dencio <dencio@macbook-pro.lan>
2019-11-28 12:54:13 +1300

    Updates

---
+++ precache-manifest.9ef789e126906e16d7ad1ad4b95ecacd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a22a9662302b8aaa503be3555b077c2",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "e97070d393ad87044474",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "add32c143492220544c6",
+    "url": "/robofriends/static/css/main.b81f75ea.chunk.css"
+  },
+  {
+    "revision": "e97070d393ad87044474",
+    "url": "/robofriends/static/js/2.814c5b91.chunk.js"
+  },
+  {
+    "revision": "add32c143492220544c6",
+    "url": "/robofriends/static/js/main.527604f6.chunk.js"
+  },
+  {
+    "revision": "7b4f849e958ac4107787",
+    "url": "/robofriends/static/js/runtime-main.a1071882.js"
+  },
+  {
+    "revision": "54c761ccedaaa0c52f3833ab18263e52",
+    "url": "/robofriends/static/media/bot.bold.54c761cc.ttf"
+  }
+]);
